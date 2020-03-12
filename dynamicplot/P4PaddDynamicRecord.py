d9cee6873b01ad4259bb453e414421a4c02e2c50
mrkraimer <mrkraimer@comcast.net>
2020-03-12 15:03:39 -0400

    make P4PaddDynamicRecord.py work

--- dynamicplot/P4PaddDynamicRecord.py
+++ dynamicplot/P4PaddDynamicRecord.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python
-from Dynamic_Viewer import ChannelStructure,Dynamic_Channel_Provider
+from Dynamic_Common import getAddRecordName,getDynamicRecordName
 from p4p.client.thread import Context,Type,Value
 import numpy as np
 ctxt = Context('pva')
-pvAddRecord = ctxt.get('PVRaddRecord')
+pvAddRecord = ctxt.get(getAddRecordName())
 print('pvAddRecord=',pvAddRecord)
-channelStructure = ChannelStructure()
-print('Dynamic_Channel_Provider().getChannelName()',Dynamic_Channel_Provider().getChannelName())
-struct = channelStructure.get()
-print('struct=',struct)
-pvAddRecord['argument']['recordName'] = Dynamic_Channel_Provider().getChannelName()
+
+struct = Value(Type([
+   ('name', 's'),
+   ('x', 'ad'),
+   ('y', 'ad'),
+   ('xmin', 'd'),
+   ('xmax', 'd'),
+   ('ymin', 'd'),
+   ('ymax', 'd'),
+]))
+
+pvAddRecord['argument']['recordName'] = getDynamicRecordName()
 pvAddRecord['argument']['union'] = struct
-ctxt.put('PVRaddRecord',)
+ctxt.put(getAddRecordName(),pvAddRecord)
 
 
