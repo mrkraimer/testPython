3828c01b8974278bfc28def6e93a8f6ac0675821
mrkraimer <mrkraimer@comcast.net>
2020-04-04 06:39:05 -0400

    make siniasa version default

--- dynamicplot/PVAPY_Dynamic_Viewer.py
+++ dynamicplot/PVAPY_Dynamic_Viewer.py
@@ -4,52 +4,58 @@ from Dynamic_Viewer import Dynamic_Viewer
 from Dynamic_Common  import Dynamic_Channel_Provider,getDynamicRecordName,DynamicRecordData
 from pvaccess import *
 import sys
-import time
+from threading import Event
 from PyQt5.QtWidgets import QApplication
-from PyQt5.QtCore import QObject
+from PyQt5.QtCore import QObject,pyqtSignal
+import numpy as np
 
 class PVAPYProvider(QObject,Dynamic_Channel_Provider) :
+    callbacksignal = pyqtSignal()
     def __init__(self):
+        self.channel = Channel(getDynamicRecordName())
         QObject.__init__(self)
         Dynamic_Channel_Provider.__init__(self)
-        self.monitorRateOnly = False
-        self.ncallbacks = 0
-        self.lastTime = time.time() 
+        self.callbacksignal.connect(self.mycallback)
+        self.callbackDoneEvent = Event()
+        self.callbackDoneEvent.set()
+        self.data = None
 
-    def start(self) :
-        self.channel = Channel(getDynamicRecordName())
-        self.channel.monitor(self.mycallback,'field()')
+    def start(self) : self.channel.monitor(self.pvapycallback,'field()')
     def stop(self) :
         self.channel.stopMonitor()
     def done(self) :
         pass
-    def mycallback(self,struct) :
-        if self.monitorRateOnly :
-            self.ncallbacks += 1
-            timenow = time.time() 
-            timediff = timenow - self.lastTime
-            if timediff<1 : return
-            print('rate=',round(self.ncallbacks/timediff))
-            self.lastTime = timenow
-            self.ncallbacks = 0
-            return
-        arg = dict()
-        try :
-            data = DynamicRecordData()
-            data.name = struct['name']
-            data.x = struct['x']
-            data.y = struct['y']
-            data.xmin = struct['xmin']
-            data.xmax = struct['xmax']
-            data.ymin = struct['ymin']
-            data.ymax = struct['ymax']
-            arg = dict()
-            arg['value'] = data
-            self.viewer.callback(arg)
-            return
-        except Exception as error:
-            arg["exception"] = repr(error)
-            self.viewer.callback(arg)
+    def callback(self,arg) :
+        self.viewer.callback(arg)
+    def pvapycallback(self,arg) :
+        data = DynamicRecordData()
+        data.name = arg['name']
+        data.x = np.copy(arg['x'])
+        data.y = np.copy(arg['y'])
+        data.xmin = arg['xmin']
+        data.xmax = arg['xmax']
+        data.ymin = arg['ymin']
+        data.ymax = arg['ymax']
+        if not self.data:
+            self.data = data
+            self.callbackDoneEvent.clear()
+            self.callbacksignal.emit()
+        else:
+            self.data = data
+
+    def mycallback(self) :
+        while self.data is not None:
+            try:
+                arg = dict()
+                arg['value'] = self.data
+                self.data = None
+                self.callback(arg)
+            except Exception as error:
+                arg["exception"] = repr(error)
+                self.callback(arg)
+        self.callbackDoneEvent.set()
+        return
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
@@ -61,4 +67,5 @@ if __name__ == '__main__':
         PVAPYProvider.setChannelName(channelName)
     PVAPYProvider.viewer = Dynamic_Viewer(PVAPYProvider,"PVAPY")
     sys.exit(app.exec_())
+
 
