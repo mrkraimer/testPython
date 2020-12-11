96a0b8f1f091a569d8a78ea0125e72c88b0a883c
mrkraimer <mrkraimer@comcast.net>
2020-12-11 10:37:08 -0500

    more work on difgeom

--- difgeom/elipse2d.py
+++ difgeom/elipse2d.py
@@ -4,27 +4,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QWidget,QLabel,QLineEdit,QSlider
+from PyQt5.QtWidgets import QPushButton,QHBoxLayout,QVBoxLayout,QGridLayout,QGroupBox
+from PyQt5.QtWidgets import QRubberBand
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QWidget
+from PyQt5.QtCore import QObject,QPoint,QRect,QSize
 
 class Ellipse() :
-    def __init__(self,a,b,c):
-        self.a = a
-        self.b = b
-        self.c = c
-    def show(self) : 
-        a = self.a
-        b = self.b
-        c = self.c
+    def __init__(self):
+        pass
+ 
+    def show(self,xmin,ymax,nrot) : 
         min = 0.0
         max = 2*np.pi
         npts = 500
         inc = (max-min)/npts
         t = np.arange(min, max, inc)
-        limit = a
-        if b>a : limit = b
+        limit = xmax
+        if ymax>xmax : limit = ymax
         plt.xlim(-limit,limit)
         plt.ylim(-limit,limit)
-        x = a*np.cos(t*c)
-        y = b*np.sin(t*c)
+        x = xmax*np.cos(t*nrot)
+        y = ymax*np.sin(t*nrot)
 
         plt.xlim(-limit,limit)
         plt.ylim(-limit,limit)
@@ -33,10 +35,10 @@ class Ellipse() :
         plt.xlabel("value")
         plt.title("ellipse")
 
-        dx = -a*c*np.sin(t*c)
-        dy = b*c*np.cos(t*c)
-        d2x = -a*c*c*np.cos(t*c)
-        d2y = -b*c*c*np.sin(t*c)
+        dx = -xmax*nrot*np.sin(t*nrot)
+        dy = ymax*nrot*np.cos(t*nrot)
+        d2x = -xmax*nrot*nrot*np.cos(t*nrot)
+        d2y = -ymax*nrot*nrot*np.sin(t*nrot)
 
         num = dx*d2y - d2x*dy
         deom = (dx*dx + dy*dy)**(3/2)
@@ -46,20 +48,79 @@ class Ellipse() :
         ax.plot(t,radius)
         ax.set_title('radius of curvature')
         ax.set(xlabel="radians")
-
         plt.show()
+
+class Viewer(QWidget) :
+    def __init__(self,xmax,ymax,nrot,parent=None):
+        super(QWidget, self).__init__(parent)
+        self.xmax = xmax
+        self.ymax = ymax
+        self.nrot = nrot
+        self.ellipse = Ellipse()
+        self.displayButton = QPushButton('display')
+        self.displayButton.setEnabled(True)
+        self.displayButton.clicked.connect(self.display)
+
+        xmaxLabel = QLabel("xmax:")
+        self.xmaxText = QLineEdit()
+        self.xmaxText.setEnabled(True)
+        self.xmaxText.setText(str(self.xmax))
+        self.xmaxText.editingFinished.connect(self.xmaxEvent)
+
+        ymaxLabel = QLabel("ymax:")
+        self.ymaxText = QLineEdit()
+        self.ymaxText.setEnabled(True)
+        self.ymaxText.setText(str(self.ymax))
+        self.ymaxText.editingFinished.connect(self.ymaxEvent)
+
+        nrotLabel = QLabel("nrot:")
+        self.nrotText = QLineEdit()
+        self.nrotText.setEnabled(True)
+        self.nrotText.setText(str(self.nrot))
+        self.nrotText.editingFinished.connect(self.nrotEvent)
+        
+        box = QHBoxLayout()
+        box.addWidget(self.displayButton)
+        box.addWidget(self.xmaxText)
+        box.addWidget(self.ymaxText)
+        box.addWidget(self.nrotText)
+        self.setLayout(box)
+        self.show()
+        
+
+    def xmaxEvent(self) :
+        try:
+            self.xmax = float(self.xmaxText.text())
+        except Exception as error:
+            self.statusText.setText(str(error))
+
+    def ymaxEvent(self) :
+        try:
+            self.ymax = float(self.ymaxText.text())
+        except Exception as error:
+            self.statusText.setText(str(error))
+
+    def nrotEvent(self) :
+        try:
+            self.nrot = float(self.nrotText.text())
+        except Exception as error:
+            self.statusText.setText(str(error))
+
+    def display(self):
+        self.ellipse.show(self.xmax,self.ymax,self.nrot)
+
+    def closeEvent(self, event) :
+        QApplication.closeAllWindows()
+    
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    a = 1
-    b = 2
-    c = 1
+    xmax = 1
+    ymax = 2
+    nrot = 1
     nargs = len(sys.argv)
     if nargs >= 2: a = float(sys.argv[1])
     if nargs >= 3: b = float(sys.argv[2])
     if nargs >= 4: c = float(sys.argv[3])
-    ellipse = Ellipse(a,b,c)
-    ellipse.show()
-    junk = input("enter anything to exit")
-    exit()
+    viewer = Viewer(xmax,ymax,nrot)
     sys.exit(app.exec_())
