ee5c0752c2a0556bc1936594235c1280eb22dfdc
mrkraimer <mrkraimer@comcast.net>
2021-06-02 07:37:08 -0400

    use gradient

--- difgeom/spiral2d.py
+++ difgeom/spiral2d.py
@@ -11,7 +11,7 @@ class Spiral() :
     def __init__(self):
         pass
  
-    def show(self,xmax,ymax,nrot) : 
+    def curve(self,xmax,ymax,nrot) : 
         plt.close('all')
         # r is radians
         npts = 500
@@ -28,23 +28,41 @@ class Spiral() :
         x = maxx*t*np.cos(t)
         maxy = ymax/rmax
         y = maxy*t*np.sin(t)
-        plt.axes().set_aspect('equal')
+        #plt.axes().set_aspect('equal')
         plt.plot(x, y,scalex=False,scaley=False)
         plt.xlabel("value")
         plt.title("spiral")
+        plt.show()
 
-        dx = maxx*np.cos(t) -t*maxx*np.sin(t)
-        dy = maxy*np.sin(t) + t*maxy*np.cos(t)
-        d2x = - maxx*np.sin(t) - maxx*np.sin(t) - maxx*np.cos(t)
-        d2y = maxy*np.cos(t)  + maxy*np.cos(t) + -t*maxy*np.sin(t)
-        
+    def curvature(self,xmax,ymax,nrot) : 
+        plt.close('all')
+        # r is radians
+        npts = 500
+        rmax = 2*np.pi*nrot
+        dr = rmax/npts
+        t = np.arange(0, rmax, dr)
+        #x = xmax*(t/rmax)*np.cos(t)
+        #x = (xmax/rmax)*t*np.cos(t)
+        maxx = xmax/rmax
+        x = maxx*t*np.cos(t)
+        maxy = ymax/rmax
+        y = maxy*t*np.sin(t)
+
+        dx = np.gradient(x)
+        dy = np.gradient(y)
+        d2x = np.gradient(dx)
+        d2y = np.gradient(dy)
         num = np.absolute(dx*d2y - d2x*dy)
         deom = (dx*dx + dy*dy)**(3/2)
         curvature = num/deom
-        f, ax = plt.subplots()
-        ax.plot(t,curvature)
-        ax.set_title('curvature')
-        ax.set(xlabel="radians")
+        curvature[0] = curvature[2]
+        curvature[1] = curvature[2]
+        curvature[npts-1] = curvature[npts-3]
+        curvature[npts-2] = curvature[npts-3]
+        plt.plot(t,curvature)
+        plt.title("curvature")
+        plt.xlabel("radians")
+      
         radius = 1/curvature
         f, ax = plt.subplots()
         ax.plot(t,radius)
@@ -59,9 +77,12 @@ class Viewer(QWidget) :
         self.ymax = ymax
         self.nrot = nrot
         self.spiral = Spiral()
-        self.displayButton = QPushButton('display')
-        self.displayButton.setEnabled(True)
-        self.displayButton.clicked.connect(self.display)
+        self.curveButton = QPushButton('curve')
+        self.curveButton.setEnabled(True)
+        self.curveButton.clicked.connect(self.curve)
+        self.curvatureButton = QPushButton('curvature')
+        self.curvatureButton.setEnabled(True)
+        self.curvatureButton.clicked.connect(self.curvature)
 
         xmaxLabel = QLabel("xmax:")
         self.xmaxText = QLineEdit()
@@ -82,7 +103,8 @@ class Viewer(QWidget) :
         self.nrotText.editingFinished.connect(self.nrotEvent)
         
         box = QHBoxLayout()
-        box.addWidget(self.displayButton)
+        box.addWidget(self.curveButton)
+        box.addWidget(self.curvatureButton)
         box.addWidget(xmaxLabel)
         box.addWidget(self.xmaxText)
         box.addWidget(ymaxLabel)
@@ -111,8 +133,11 @@ class Viewer(QWidget) :
         except Exception as error:
             self.statusText.setText(str(error))
 
-    def display(self):
-        self.spiral.show(self.xmax,self.ymax,self.nrot)
+    def curve(self):
+        self.spiral.curve(self.xmax,self.ymax,self.nrot)
+        
+    def curvature(self):
+        self.spiral.curvature(self.xmax,self.ymax,self.nrot)
 
     def closeEvent(self, event) :
         QApplication.closeAllWindows()
